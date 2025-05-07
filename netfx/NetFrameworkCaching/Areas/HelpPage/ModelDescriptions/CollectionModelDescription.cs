3fd85774d472ced4ad1053e05d3e4bee3adb6998
Ryan Lintag <ryanlintag@yahoo.com>
2025-05-08 00:03:24 +0800

    Base project for .NET 9 and .NET Framework 4.8.1

---
+++ netfx/NetFrameworkCaching/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace NetFrameworkCaching.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
